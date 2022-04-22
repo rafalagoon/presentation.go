869e39b7a5f5aa0d95543732f3efe8f4c7137cdf
Rafa Laguna <rafa@electrojocs.com>
2022-04-22 07:06:59 +0200

    #1 Animations on visible play in a loop

--- scenes/Slide.gd
+++ scenes/Slide.gd
@@ -6,6 +6,8 @@ class_name Slide extends Node2D
 export (String) var title = "[Title]"
 
 
+var is_playing = false
+
 
 func _ready():
 	if has_node("AnimationPlayer"):
@@ -16,11 +18,12 @@ func _process(delta):
 	$Title.text = title
 
 	if is_visible_in_tree():
-		if has_node("AnimationPlayer"):
+		if has_node("AnimationPlayer") and not is_playing:
 			get_node("AnimationPlayer").play("Visible")
-			
+			is_playing = true
 	else:
 		if has_node("AnimationPlayer"):
 			get_node("AnimationPlayer").seek(0)
+			is_playing = false
 
 
